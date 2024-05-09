fd85f81bc7cac2f36a8e85876b5fc57b79a2b452
JDMADESP <152448238+JDMADESP@users.noreply.github.com>
2024-05-09 14:46:29 -0400

    Added new bot that checks tone and new functions

--- openai_test.py
+++ openai_test.py
@@ -1,12 +1,13 @@
 
 from dotenv import load_dotenv
+import sys
 import argparse
 import os
 from openai import OpenAI
 
 
 
-def relay_message(message):
+def relay_message(message: str | None, tone: str | None) -> (str):
   load_dotenv('.env')
   key = os.getenv('API_KEY')
   # ast = os.getenv('AST_KEY')
@@ -14,18 +15,55 @@ def relay_message(message):
   completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are an editor who is in charge of changing the tones of phrases to the specified new tone."},
-    {"role": "user", "content": message}
+    {"role": "system", "content": "You are an editor who is in charge of" +
+                                   "changing the tones of phrases to the" +
+                                   "specified new tone."},
+    {"role": "user", "content": ("Using a " + tone + "tone change the sentence"
+                                 "Change:" + message)}
   ]
 )
   return completion.choices[0].message.content
 
+def check_if_tone(tone_test):
+  load_dotenv('.env')
+  key = os.getenv('API_KEY')
+  client = OpenAI(api_key=key)
+  completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": ("You are an editor who is in charge of" + 
+                                   "deciding if a bunch of input words or a word" + 
+                                   "describe a type of tone." + "A few examples of" +
+                                   "tones would be a happy, casual, sad, formal."
+                                   "If the words describe a tone, just respond with: yes")},
+    {"role": "user", "content": tone_test}
+  ]
+)
+  tone_return = completion.choices[0].message.content
+  print(tone_return)
+  if tone_return == "Yes":
+    return True
+  else:
+    return False
+  
+
 def main():
+  if len(sys.argv) != 3:
+    print("Not Enough arguments or too many arguments")
+    return
   parser = argparse.ArgumentParser(description='Tone Editor')
-  parser.add_argument('message', type=str, help='Sentence and Tone to Change')
+  parser.add_argument('message', type=str, help='Sentence')
+  parser.add_argument('tone', type=str,help='Tone')
   args = parser.parse_args()
-  message_sending = args.message
-  response = relay_message(message_sending)
+  tone_check = check_if_tone(args.tone)
+  print(tone_check)
+  if tone_check: #if second argument is a tone
+    message_sending = args.message
+    tone_sending = args.tone
+  else:
+    print("Your second argument is not a tone")
+    return
+  response = relay_message(message_sending, tone_sending)
   print(response)        
 
 
